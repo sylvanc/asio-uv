3205492477eb079e3b917824de95f740e78e5158
Sylvan Clebsch <sylvan.clebsch@microsoft.com>
2026-04-17 09:00:38 -0500

    WIP: add tcp echo test for debugging

--- main.v
+++ main.v
@@ -114,5 +114,48 @@ main(): none
     t 0
   }
 
+  // ===== tcp tests =====
+
+  (test "tcp - echo") tc ->
+  {
+    let a = addr("127.0.0.1", 9123);
+
+    let server = tcp_listener(a).start
+      (s: tcp_listener::_state, conn: tcp) ->
+    {
+      conn.start (conn: stream_read, data: array[u8], size: usize) ->
+      {
+        if size > 0
+        {
+          conn.write(data, size)
+        }
+        else
+        {
+          conn.close
+        }
+      }
+    }
+
+    let t = timer (t: timer::_state) ->
+    {
+      let client = tcp(a).start
+        (conn: stream_read, data: array[u8], size: usize) ->
+      {
+        if size > 0
+        {
+          tc.assert(size == 4, "echo should return 4 bytes");
+          conn.close;
+          server.close
+        }
+        else
+        {
+          conn.close
+        }
+      }
+      client.write(array[u8]::fill(4, 72))
+    }
+    t 10
+  }
+
   test.done
 }
